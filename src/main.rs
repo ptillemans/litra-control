9f97f59276d3c81483d70b12421322f9803b6d37
ptillemans <pti@snamellit.com>
2022-10-12 11:44:32 +0200

    add brightness and temperature control

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::ffi::CString;
-use std::time::Duration;
 use anyhow::{Result, Context};
 use hidapi::{HidApi, DeviceInfo, HidDevice};
 use clap::{Parser, Subcommand};
@@ -43,30 +42,68 @@ enum Commands {
     Init,
     On,
     Off,
+    Brightness {
+        #[arg(value_name="PERCENT")]
+        percent: u16
+    },
+    Temperature {
+        #[arg(value_name="TEMPERATURE")]
+        temperature: u16
+    }
 }
 
 
 
-fn find_device_path() -> anyhow::Result<()>{
-    Ok(())
+fn find_device_path(api: &mut HidApi) -> Result<String>{
+
+    api.refresh_devices().context("Refreshing devices")?;
+    let devices = api.device_list().filter(|dev| is_litra_device(dev));
+
+    //for device in devices {
+    //    println!("{:04x}:{:04x} {:?} -> {:?}", device.vendor_id(), device.product_id(), device.product_string(), device.path());
+    //}
+
+    // experience shows it is the second device
+    // TODO: find better way to find the device(s) we need as this will not work with
+    // multiple lamps
+    let device = devices.last().context("Getting last litra device")?;
+    device.path().to_str().context("Converting Litra device path")
+        .map(|s| String::from(s))
 }
 
-fn light_on(device: &HidDevice) -> anyhow::Result<()> {
-    println!("Turn Litra on.");
-    device.write(&[0x11, 0xff, 0x04, 0x1e, 0x01])
+fn send_command(device: &HidDevice, command: u8, argument: u16) -> Result<()> {
+    let low_byte:u8  = (argument & 0xff) as u8;
+    let high_byte:u8  = (argument >> 8 & 0xff) as u8;
+    let buf = [0x11, 0xff, 0x04, command, high_byte, low_byte];
+    device.write(&buf)
         .map(|_| ())
+        .context(format!("Sending command {:02x}", command))
+}
+
+fn light_on(device: &HidDevice) -> Result<()> {
+    println!("Turn Litra on.");
+    send_command(device, 0x1c, 0x0100)
         .context("Turning light on")
 }
 
-fn light_off(device: &HidDevice) -> anyhow::Result<()> {
+fn light_off(device: &HidDevice) -> Result<()> {
     println!("Turn Litra off.");
-    device.write(&[0x11, 0xff, 0x04, 0x1e, 0x00])
-        .map(|_| ())
+    send_command(device, 0x1c, 0x0100)
         .context("Turning light off")
 }
 
+const MIN_BRIGHTNESS: u16 = 0x14;
+const MAX_BRIGHTNESS: u16 = 0xfa;
+fn set_brightness(device: &HidDevice, percent: u16) -> Result<()>{
+    let brightness = MIN_BRIGHTNESS + (MAX_BRIGHTNESS - MIN_BRIGHTNESS) * percent / 100;
+    send_command(device, 0x4c, brightness)
+}
 
-fn main() -> anyhow::Result<()> {
+fn set_temperature(device: &HidDevice, temperature: u16) -> Result<()> {
+   send_command(device, 0x9c, temperature)
+}
+
+fn main() -> Result<()> {
 
     let cli = Cli::parse();
 
@@ -75,12 +112,15 @@ fn main() -> anyhow::Result<()> {
     }
 
     let path = "\\\\?\\HID#VID_046D&PID_C900&Col02#a&8fac6bd&0&0001#{4d1e55b2-f16f-11cf-88cb-001111000030}";
-    let api = HidApi::new_without_enumerate().context("Creating HidApi.")?;
+    let mut api = HidApi::new_without_enumerate().context("Creating HidApi.")?;
     let device = api.open_path(&CString::new(path).unwrap()).context("Opening connection to Litra")?;
 
     match &cli.command {
         Some(Commands::Init) => {
-            find_device_path()
+            let path = find_device_path(&mut api).context("Find device path")?;
+            println!("Path : {}", path);
+            println!("Tip: set the environment variable LITRA_PATH to this value to avoid enumeration of devices.");
+            Ok(())
         },
         Some(Commands::On) => {
             light_on(&device)
@@ -88,15 +128,16 @@ fn main() -> anyhow::Result<()> {
         Some(Commands::Off) => {
             light_off(&device)
         },
+        Some(Commands::Brightness {percent}) => {
+            set_brightness(&device, *percent)
+        },
+        Some(Commands::Temperature {temperature}) => {
+            set_temperature(&device, *temperature)
+        }
         None => {
             println!("No command given.");
             Ok(())
         }
     }
 
-            // api.refresh_devices().unwrap();
-            // let devices = api.device_list().filter(|dev| is_litra_device(dev));
-            //
-            // for device in devices {
-            //    println!("{:04x}:{:04x} {:?} -> {:?}", device.vendor_id(), device.product_id(),device.product_string(), device.path());
 }
